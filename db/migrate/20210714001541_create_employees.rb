4b48a4f0f1d0dfd586ee598105404f0ebbe61197
Jean Tessier <jean.tessier@gusto.com>
2021-07-13 17:16:50 -0700

    Add models
        $ bin/rails g model Employee company:references
        $ bin/rails g model Company
        $ bin/rails db:migrate

---
+++ db/migrate/20210714001541_create_employees.rb
@@ -0,0 +1,9 @@
+class CreateEmployees < ActiveRecord::Migration[6.1]
+  def change
+    create_table :employees do |t|
+      t.references :company, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
