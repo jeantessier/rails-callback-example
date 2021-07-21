4870a376651cafa514d3d43d934623775e3b2ec4
Jean Tessier <jean.tessier@gusto.com>
2021-07-21 14:06:47 -0700

    Add simple model to test after_commit callbacks
        $ bin/rails g model User name:string age:integer
        $ bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_001605) do
+ActiveRecord::Schema.define(version: 2021_07_21_210533) do
 
   create_table "companies", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2021_07_14_001605) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["company_id"], name: "index_employees_on_company_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "employees", "companies"
